3d017227168062bd644cb1077f76f773c54792b3
SiddharthM18 <siddharthlego12@gmail.com>
2022-03-14 12:18:33 -0400

    added devops page

---
+++ pages/tools/devops.js
@@ -0,0 +1,49 @@
+import Head from 'next/Head';
+import Link from 'next/Link';
+import Image from 'next/Image';
+import Layout, {siteTitle} from '../../components/layout';
+import utilStyles from '../../styles/utils.module.css'
+
+
+export default function PythonTest() {
+    return (
+        <Layout>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <main className={utilStyles.main}>
+                <h1 className={utilStyles.headingXL}>
+                    Select a Development Tool
+                </h1>
+                <div className={utilStyles.grid}>
+                    <div className={utilStyles.devopscard}>
+                        <Link href='https://coinsvt.atlassian.net/jira/software/projects/QDS22/boards/1'>
+                            <a>
+                                <Image
+                                    priority
+                                    src='/images/jira.png'
+                                    className={utilStyles.jiraimage}
+                                    height={200}
+                                    width={200}
+                                />
+                            </a>
+                        </Link>
+                    </div>
+                    <div className={utilStyles.devopscard}>
+                        <Link href='https://github.com/Commodity-Investing-by-Students'>
+                            <a>
+                                <Image
+                                    priority
+                                    src='/images/github.png'
+                                    className={utilStyles.githubimage}
+                                    height={200}
+                                    width={200}
+                                />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </main>
+        </Layout>
+    )
+}
